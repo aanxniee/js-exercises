ccbbf8b3d366277eff7620164b8d47355af3d4a1
Annie Cai <caishajia@gmail.com>
2023-01-04 21:21:41 -0500

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(beginning, end) {
+    var sum = 0;
+    if (!Number.isFinite(beginning) || !Number.isFinite(end)) {
+        return "ERROR";
+    }
+    
+    if (beginning < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    if (beginning < end) {
+        for (i = beginning; i <= end; i++) {
+            sum = sum + i;
+        }
+    }
+    else {
+        for (i = end; i <= beginning; i++) {
+            sum = sum + i;
+        }
+    }
+
+    return sum;
 };
-
+console.log(sumAll('0', 1));
 // Do not edit below this line
 module.exports = sumAll;
