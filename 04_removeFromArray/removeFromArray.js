94c13dd0fb7b941db9d4789cf5a7a421878ae3d9
Annie Cai <caishajia@gmail.com>
2023-01-04 20:49:11 -0500

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    const arr = args[0];
+    const newArr = [];
+    arr.forEach((ele) => {
+        if (!args.includes(ele)) {
+            newArr.push(ele);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
