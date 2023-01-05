6edba8fcd71a6242715508470b919b9185a99663
Annie Cai <caishajia@gmail.com>
2023-01-04 20:32:53 -0500

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-    
+const repeatString = function(string, num) {
+    if (num > 0) {
+        return string.repeat(num);
+    }
+    else if (num == 0) {
+        return '';
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
