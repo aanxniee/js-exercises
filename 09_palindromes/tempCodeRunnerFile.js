f604d365aa0ca91c23157774efb04c225ffdf95b
Annie Cai <caishajia@gmail.com>
2023-01-06 16:16:31 -0500

    completed exercise 9

--- 09_palindromes/tempCodeRunnerFile.js
+++ 09_palindromes/tempCodeRunnerFile.js
@@ -3,13 +3,13 @@ const palindromes = function (str) {
 
     let start = 0;
     let end = newStr.length - 1;
-    while (start < end) {
-        if (newStr[start++] != newStr[end--]) {
-            return false;
-        }
+    while (newStr[start++] != newStr[end--]) {
+        return false;
     }
     return true;
 };
 
+console.log('ZZZZ car, a man, a maracaz.'.replace(/\s/g, "").replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase());
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
 // Do not edit below this line
 module.exports = palindromes;
