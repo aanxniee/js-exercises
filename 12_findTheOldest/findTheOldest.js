295bdbf4fd1fcc6ea87f5111a2e40f0f39f8ec74
Annie Cai <caishajia@gmail.com>
2023-01-06 16:40:47 -0500

    completed exercise 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(p) {
+    return p.reduce((oldest, current) => {
+        const oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = findAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currentAge ? current : oldest;
+    })
+};
 
+const findAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
